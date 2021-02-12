4f3c334c75c747a317358469ad48493b10b90ce1
Ashley Thibodeau <AThibodeau1@Student.Fullsail.edu>
2021-02-12 10:44:22 -0500

    addtion of event creation page

---
+++ Thibodeau_Ashley_CE04/Thibodeau_Ashley_CE04/EventCreationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Thibodeau_Ashley_CE04
+{
+    public partial class EventCreationPage : ContentPage
+    {
+        public EventCreationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Thibodeau_Ashley_CE04/Thibodeau_Ashley_CE04/Models/EventDetails.cs
+++ Thibodeau_Ashley_CE04/Thibodeau_Ashley_CE04/Models/EventDetails.cs
@@ -3,8 +3,9 @@ namespace Thibodeau_Ashley_CE04.Models
 {
     public class EventDetails
     {
-        public EventDetails()
-        {
-        }
+        public string Image { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+
     }
 }
